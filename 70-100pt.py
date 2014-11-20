3dc42c2e7e999e2cefeeeac1e50c5eda78f29927
CarlosTheDragonSlayer <carlos.ojeda.gymnastics@gmail.com>
2014-11-20 13:24:41 -0600

    85

--- 70-100pt.py
+++ 70-100pt.py
@@ -11,55 +11,81 @@
 
 from Tkinter import *
 root = Tk()
-
+ 
 drawpad = Canvas(root, width=800,height=600, background='white')
-player = drawpad.create_oval(390,580,410,600, fill="green")
-enemy1 = drawpad.create_rectangle(50,100,70,20, fill="red")
-enemy2 = drawpad.create_rectangle(390,250,410,270, fill="red")
-enemy3 = drawpad.create_rectangle(780,400,800,420, fill="red")
-
-direction = 5
-direction2 = 7
-direction3 = -9
-
-
-class MyApp:
-	def __init__(self, parent):
-       	    global drawpad
+player = drawpad.create_oval(390,580,410,600, fill="red")
+opponent = drawpad.create_rectangle(490,580,410,600, fill="red")
+player = drawpad.create_rectangle(390,580,410,600, fill="blue")
+ 
+ # Create your "enemies" here, before the class
+def __init__(self, parent):
+        	    # Bind an event to the first button
+            self.up.bind("<Button-1>", self.upClicked)
+        	    
+      	    self.myParent = parent  
+     	    self.myContainer1 = Frame(parent)
+       	    self.myContainer1.pack()
+       	    self.down = Button(self.myContainer1)
+       	    self.down.configure(text="Down", background= "Blue")
+       	    self.down.grid(row=0,column=1)
+       	    # Bind an event to the first button
+       	    self.down.bind("<Button-1>", self.downClicked)
+       	    
        	    self.myParent = parent  
        	    self.myContainer1 = Frame(parent)
        	    self.myContainer1.pack()
+       	    self.right = Button(self.myContainer1)
+       	    self.right.configure(text="right", background= "Red")
+       	    self.right.grid(row=0,column=1)
+       	    # Bind an event to the first button
+       	    self.right.bind("<Button-1>", self.rightClicked)
        	    
-       	    self.up = Button(self.myContainer1)
-       	    self.up.configure(text="up", background= "green")
-       	    self.up.grid(row=0,column=0)
-       	    self.up.bind("<Button-1>", self.upClicked)
-    
-       	    drawpad.pack(side=BOTTOM)
+       	    self.myParent = parent  
+       	    self.myContainer1 = Frame(parent)
+       	    self.myContainer1.pack()
+       	    self.left = Button(self.myContainer1)
+       	    self.left.configure(text="left", background= "purple")
+       	    self.left.grid(row=0,column=1)
+       	    # Bind an event to the first button
+       	    self.left.bind("<Button-1>", self.leftClicked)
        	    
-       	    self.bottom = Button(self.myContainer1)
-       	    self.bottom.configure(text="bottom", background= "blue")
-       	    self.bottom.grid(row=2,column=1)
-       	    self.bottom.bind("<Button-1>", self.downClicked)
-    
+       	    # No need to edit this - just includes the drawpad into our frame
+       	    drawpad.pack(side=RIGHT)
+       	    # call the animate function to start our recursion
        	    self.animate()
-	
-	def animate(self):
-	    global drawpad
-	    global player
+       	    drawpad.pack(side=BOTTOM)
+
+def animate(self):
+	       global drawpad
+	       global player
 	    # Remember to include your "enemies" with "global"
 	    
 	    # Uncomment this when you're ready to test out your animation!
 	    #drawpad.after(10,self.animate)
+	    	
 		
-	def upClicked(self, event):   
+def upClicked(self, event):   
 	   global oval
 	   global player
 	   drawpad.move(player,0,-20)
-        def downClicked(self, event):   
+		
+
+def downClicked(self, event):   
 	   global oval
 	   global player
-	   drawpad.move(player,0,20)	
-
+	   drawpad.move(player,0,20)
+	 
+def rightClicked(self, event):   
+	   global oval
+	   global player
+	   drawpad.move(player,20,0)
+	
+def leftClicked(self, event):   
+	   global oval
+	   global player
+	   drawpad.move(player,-20,0)
+		
+		
 app = MyApp(root)
+root.mainloop()
 root.mainloop()
